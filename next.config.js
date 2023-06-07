a3faaa155dd43938b1a37c82a968607dad79e13c
ronrix <ronrixl5@gmail.com>
2023-06-07 20:07:52 +0800

    add domain to nextjs config and adding libraries like mui and clsx

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['www.themoviedb.org'],
+        domains: ['www.themoviedb.org', 'image.tmdb.org'],
   },
 }
 
